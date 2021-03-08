f74cc04299b8c1888cc02cbab7e5ec8185ac0596
Gabriele Falloon <ggfalloon@gmail.com>
2021-03-08 16:39:29 -0600

    indexeddb functioning properly

--- public/db.js
+++ public/db.js
@@ -18,3 +18,35 @@ request.onsuccess = (event) => {
         checkDatabase();
     }
 };
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+}
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            }).then(response => response.json())
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
+}
+
+window.addEventListener("online", checkDatabase);
