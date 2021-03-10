d6878b11dbd72fe2f65776eb0868c3d581b8d2e2
Gabriele Falloon <ggfalloon@gmail.com>
2021-03-09 22:37:59 -0600

    correctted error

--- public/service-worker.js
+++ public/service-worker.js
@@ -22,18 +22,18 @@ self.addEventListener("install", event => {
 });
 
 // The activate handler takes care of cleaning up old caches.
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
     const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
     event.waitUntil(
         caches
             .keys()
-            .then(cacheNames => {
+            .then((cacheNames) => {
                 // return array of cache names that are old to delete
                 return cacheNames.filter(
-                    cacheName => !currentCaches.includes(cacheName)
+                    (cacheName) => !currentCaches.includes(cacheName)
                 );
             })
-            .then(cachesToDelete => {
+            .then((cachesToDelete) => {
                 return Promise.all(
                     cachesToDelete.map(cacheToDelete => {
                         return caches.delete(cacheToDelete);
